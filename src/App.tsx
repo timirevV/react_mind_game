0e250fbc1556dd95960ac036b40db0ff2150fc94
timirevV <timirev.fortech@gmail.com>
2025-10-08 12:59:44 +0300

    fix: remove router v6 and add v7

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import StartPage from "./pages/StartPage";
 import MainPage from "./pages/MainPage";
 import ResultsPage from "./pages/ResultsPage";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <StartPage />,
+  },
+  {
+    path: "/main",
+    element: <MainPage />,
+  },
+  {
+    path: "/results",
+    element: <ResultsPage />,
+  },
+]);
+
 const App = () => {
   return (
-    <Router
-      future={{
-        v7_startTransition: false,
-        v7_relativeSplatPath: false,
-      }}
-    >
-      <Routes>
-        <Route path="/" element={<StartPage />} />
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/results" element={<ResultsPage />} />
-      </Routes>
-    </Router>
+    <RouterProvider router={router} />
   );
 };
 
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://example.com/api/" }),
   tagTypes: ["Leaderboard"],
   endpoints: (builder) => ({
     getLeaderboard: builder.query<{ name: string; score: number }[], void>({
